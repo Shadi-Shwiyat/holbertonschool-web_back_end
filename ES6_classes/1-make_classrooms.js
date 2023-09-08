42c745c7f170887a994884b89ec48a8d7afed2d1
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-07 20:45:10 -0500

    fixed task 1 linter issue

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms () {
-    const classroomArray = [];
-    const maxStudents = [19, 20, 34];
+export default function initializeRooms() {
+  const classroomArray = [];
+  const maxStudents = [19, 20, 34];
 
-    for (const maxNum of maxStudents) {
-        const newClassRoom = new ClassRoom(maxNum);
-        classroomArray.push(newClassRoom);
-    }
-    return classroomArray;
+  for (const maxNum of maxStudents) {
+    const newClassRoom = new ClassRoom(maxNum);
+    classroomArray.push(newClassRoom);
+  }
+  return classroomArray;
 }
