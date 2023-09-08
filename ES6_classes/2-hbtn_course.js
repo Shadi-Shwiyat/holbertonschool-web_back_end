8b10ed9d35a8615b3389db1bb685a79861c2065a
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-07 21:05:36 -0500

    Created holberton course class with type verificatin and getters/setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,2 +1,58 @@
 export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new Error('Students must be an array of strings');
+    }
+  }
 }
