68726550e536c28085a26385ec3d43dccacf549d
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-07 23:26:34 -0500

    trying to fix task 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,57 +1,9 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new Error('Brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw new Error('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw new Error('Motor must be a string');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
-
-  // Getters
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  // Setters
-  set brand(newBrand) {
-    if (typeof newBrand !== 'string') {
-      throw new Error('Brand must be a string');
-    } else {
-      this._brand = newBrand;
-    }
-  }
-
-  set motor(newMotor) {
-    if (typeof newMotor !== 'string') {
-      throw new Error('Motor must be a string');
-    } else {
-      this._motor = newMotor;
-    }
-  }
-
-  set color(newColor) {
-    if (typeof newColor !== 'string') {
-      throw new Error('Color must be a string');
-    } else {
-      this._color = newColor;
-    }
-  }
-
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
   }
