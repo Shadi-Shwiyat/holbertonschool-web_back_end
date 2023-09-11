9600b6046b07d430cba09e1f4ae0457ec36a530c
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-11 16:30:35 -0500

    Task 1 mapping id's to an array

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,13 @@
+export default function getListStudentIds(studentArray) {
+  const idArray = [];
+
+  for (const object of studentArray) {
+    for (const key in object) {
+      if (key === 'id') {
+        //console.log(object[key]);
+        idArray.push(object[key]);
+      }
+    }
+  }
+  return idArray;
+}
