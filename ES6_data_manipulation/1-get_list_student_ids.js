de3dd6d998a825c85f7bb367d745354192b093a2
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-11 16:44:27 -0500

    fixed task 1 to use map function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,7 @@
 export default function getListStudentIds(studentArray) {
-  const idArray = [];
-
-  for (const object of studentArray) {
-    for (const key in object) {
-      if (key === 'id') {
-        // console.log(object[key]);
-        idArray.push(object[key]);
-      }
-    }
+  if (!Array.isArray(studentArray)) {
+    return [];
   }
-  return idArray;
+
+  return studentArray.map((object) => object.id);
 }
