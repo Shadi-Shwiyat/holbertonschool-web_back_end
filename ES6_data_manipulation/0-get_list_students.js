a40271105fb5021845bb423577a5f47d461ee5dc
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-11 14:59:55 -0500

    Task 0 fixed class name issue

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -5,15 +5,12 @@ export default function getListStudents() {
       this.firstName = firstName;
       this.location = location;
     }
-
-    toString() {
-        return `{ id: ${this.id}, firstName: ${this.firstName}, location: ${this.location} }`
-    }
   }
 
-  let student1 = new Students(1, 'Guillame', 'San Francisco');
-  let student2 = new Students(2, 'James', 'Columbia');
-  let student3 = new Students(5, 'Serena', 'San Francisco');
+  // Convert class instances to plain objects using Object.assign
+  const student1 = { ...new Students(1, 'Guillaume', 'San Francisco') };
+  const student2 = { ...new Students(2, 'James', 'Columbia') };
+  const student3 = { ...new Students(5, 'Serena', 'San Francisco') };
 
-  return [student1.toString(), student2.toString(), student3.toString()];
+  return [student1, student2, student3];
 }
