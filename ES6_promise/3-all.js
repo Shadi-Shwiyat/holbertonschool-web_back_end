7c966060c80f2c76d28e80d73ab8139080214cbf
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-13 19:34:13 -0500

    fix error logging task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(uploadPhotoResponse.body, userResponse.firstName, userResponse.lastName);
     })
     .catch(() => {
-      console.error(new Error('Signup system offline'));
+      console.log('Signup system offline');
     });
 }
