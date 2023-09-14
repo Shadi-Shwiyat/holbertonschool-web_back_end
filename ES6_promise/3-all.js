d4b0adf94188577e8b10bb7451a8d4afe8557055
Shadi-Shwiyat <raz611103@gmail.com>
2023-09-13 19:39:51 -0500

    testing task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhotoResponse, userResponse]) => {
-      console.log(uploadPhotoResponse.body, userResponse.firstName, userResponse.lastName);
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
